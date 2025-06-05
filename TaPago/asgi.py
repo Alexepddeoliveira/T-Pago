1753f3df92ffb7f425cae6f276a5d95599e31733
Alex euzebio pereira dias de oliveira <127499015+Alexepddeoliveira@users.noreply.github.com>
2025-06-05 15:12:30 -0300

    Corrige referência para 'cardapio_site' nos arquivos do projeto

--- TaPago/asgi.py
+++ TaPago/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardapio_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaPago.settings')
 
 application = get_asgi_application()
