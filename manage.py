9b92350c0f1ebe3be68eac0ec829b397abb51a7c
Alex euzebio pereira dias de oliveira <127499015+Alexepddeoliveira@users.noreply.github.com>
2025-06-05 15:06:33 -0300

    Corrige referência a 'cardapio_site' no manage.py, wsgi e asgi

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardapio_site.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaPago.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
