9e2f1f7f0942b662095ba5562c4880de412c8d43
Alex euzebio pereira dias de oliveira <127499015+Alexepddeoliveira@users.noreply.github.com>
2025-06-05 14:59:08 -0300

    Corrige referência incorreta a 'cardapio_site'

--- TaPago/wsgi.py
+++ TaPago/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardapio_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaPago.settings')
 
 application = get_wsgi_application()
